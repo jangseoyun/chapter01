95062616dc2d31e172aa4241f929c9c231de8eaf
장서윤 <may-primavera@naver.com>
2021-11-15 17:11:34 +0900

    if~else문 예제

---
+++ src/com/javaex/ex03/Ex03.java
@@ -0,0 +1,26 @@
+package com.javaex.ex03;
+
+import java.util.Scanner;
+
+public class Ex03 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("숫자를 입력해 주세요");
+		System.out.print("숫자: ");
+		int point = sc.nextInt();
+		
+		//12 -5 0 
+		if(point>0) {
+			System.out.println("양수"); //12
+		} else if(point<0) {
+			System.out.println("음수"); //-5 0
+		} else {
+			System.out.println("0");  //0 
+		}
+		
+		sc.close();
+	}
+
+}
