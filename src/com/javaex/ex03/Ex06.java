5c535b00cf1aea17bdefdbeeb77593048e771e64
장서윤 <may-primavera@naver.com>
2021-11-16 10:27:24 +0900

    if else 연습문제3

--- src/com/javaex/ex03/Ex06.java
+++ src/com/javaex/ex03/Ex06.java
@@ -11,9 +11,9 @@ public class Ex06 {
 	
 	System.out.println("점수를 입력하세요");
 	int score = sc.nextInt();
-	int div = score%3;
+//	int div = score%3; 한줄로 코드 정리 
 	
-	if(div==0) {
+	if(score%3==0) {
 		System.out.println(score+"은(는) 3의 배수 입니다.");
 	}else {
 		System.out.println(score+"은(는) 3의 배수가 아닙니다.");
